d770ad3339efadce9a8447209cc74dde808bb9e3
Thagoo <lohitgowda56@gmail.com>
2020-11-18 13:08:33 +0000

    rova: Build prebuilt CAF liblight HAL for rolex
    rolex has RGB led light and riva has only white so we cannot use unified lights hal and this is built from latest liblight hal from CAF not stock

--- rova-vendor.mk
+++ rova-vendor.mk
@@ -24,6 +24,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/rova/proprietary/vendor/lib/hw/led/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/led/consumerir.default.so \
     vendor/xiaomi/rova/proprietary/vendor/lib64/hw/led/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/led/consumerir.default.so \
+    vendor/xiaomi/rova/proprietary/vendor/lib/hw/led/lights.msm8937.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/led/lights.msm8937.so \
+    vendor/xiaomi/rova/proprietary/vendor/lib64/hw/led/lights.msm8937.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/led/lights.msm8937.so \
     vendor/xiaomi/rova/proprietary/vendor/lib/hw/spidev/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/spidev/consumerir.default.so \
     vendor/xiaomi/rova/proprietary/vendor/lib64/hw/spidev/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/spidev/consumerir.default.so
 
